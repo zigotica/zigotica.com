9256f0678dbca8577ac2efcfe39aa238f9ae98c0
Sergi <zigotica@gmail.com>
2018-06-11 13:08:01 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+		<title>Sergi Meseguer aka zigotica</title>
+	    <style>*{margin:0;padding:0}html,body{height:100%}body{color:#4F4D49;background:#2e2d2b;font-family:sans-serif;margin:0 auto}a{color:#3343d6}@media (max-width: 599px){body{width:320px;font-size:62.5%;line-height:1.44444444444}#wrapper{width:300px;padding-top:300px}section,footer{padding:0 1em}}@media (min-width: 600px){body{width:520px;font-size:80%;line-height:1.44444444444}#wrapper{width:500px;padding-top:500px}section,footer{padding:0 5em}}#wrapper{min-height:100%;margin:0 auto;background:#eee url(img/avatar.jpg) no-repeat;background-size:100%}section{margin-bottom:3em}h1{margin-top:0.5em}h2{margin-top:1em}section h1{font-size:3.6em}section h2{font-size:2.4em;line-height:1.25}section p{font-size:1.8em;line-height:1.44444444444444444444;margin-bottom:.72222222222222222222em}.social{margin:1em auto;text-align:center}.social li{display:inline-block;margin:0 1px 5px 0}ul{margin:1em;list-style-position:inside}ul li{font-size:1.4em;line-height:1.42857142857;list-style-position:inside;margin-bottom:5px}footer{font-size:1.6em;background:#ccc}</style>
+	</head>
+
+<body>
+	<div id="wrapper">
+		<section>
+			<h1>Sergi Meseguer</h1>
+			<p>Enthusiast developer who codes front and back end solutions, helping agencies build digital products for clients from different sectors.</p>
+
+			<h2>Client work:</h2>
+			<ul>
+				<li>Front-end at <a href="http://www.windowsphone.com/es-es/store/app/aireuropa/13bab62b-a2c1-4065-b63a-6606d7fe1d90">Air Europa</a>'s Windows Phone app for <a href="http://hanzo.es/">hanzo</a></li>
+				<li>Full front-end <a href="http://www.clubatleticodemadrid.com">Atlético de Madrid</a> for <a href="http://www.quoco.com">quoco</a></li>
+				<li>Mobile optimizations and sliders <a href="http://www.vogue.es">Vogue</a> for <a href="http://hanzo.es/">hanzo</a></li>
+				<li>Front-end <a href="http://www.gincollege.com">Gin College</a> for <a href="http://pelonio.co.uk">Pelonio UK</a></li>
+				<li>Intranets or old projects for MTV Spain, National Geographic Spain, RBA and banks like BBVA, Santander or Standard Chartered.</li>
+			</ul>
+
+			<h2>Skills:</h2>
+			<ul>
+				<li>Responsive Web Design</li>
+				<li>HTML, CSS, Sass/Compass, plain vanilla JavaScript, Ajax, Zepto/jQuery</li>
+				<li>EmberJS, Backbone/Marionette, Angular, React. Also custom made frameworks.</li>
+				<li>GruntJS, JasmineJS, PhantomJS/CasperJS</li>
+				<li>Git</li>
+				<li>PHP, MySQL, Wordpress (custom as CMS and plugins), Apache/Nginx</li>
+			</ul>
+
+			<h2>Side projects:</h2>
+			<ul>
+				<li><a href="http://gridsponsive.com/">Gridsponsive</a> opinionated responsive grids boilerplate.</li>
+				<li><a href="http://embryojs.com/">embryoJS</a> mini library to replace jQuery with minimal lines of code. Inspired by Zepto.js, but quite smaller in scope and size.</li>
+				<li><a href="http://zigotica.github.io/zwipeJS/">zwipeJS</a> tiny jQuery slider with hardware accelerated CSS transitions.</li>
+				<li><a href="https://github.com/zigotica/tumblr-full-backup">tumblr-full-backup</a> exports posts, images and likes from a tumblr blog to markdown.</li>
+				<li><a href="http://smeseguer.com/">Sergi Meseguer</a>: photography-related website.</li>
+			</ul>
+
+			<h2>Find me at:</h2>
+			<ul class="social">
+				<li><a href="https://github.com/zigotica">github</a></li>
+				<li><a href="http://twitter.com/zigotica">twitter</a></li>
+				<li><a href="http://zigotica.tumblr.com/">tumblr</a></li>
+				<li><a href="http://www.linkedin.com/in/sergimeseguer">linkedin</a></li>
+			</ul>
+
+		</section>
+		<footer><small>Contents and image © 2004 - 2018, Sergi Meseguer. All rights reserved.</small></footer>
+	</div>
+</body>
+</html>
